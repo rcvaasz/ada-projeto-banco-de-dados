5fd86879977bc25167cadce26d22a697e0c725db
leonildolinck <leonildolinck@gmail.com>
2023-12-14 16:43:08 -0300

    v03
    Correções e inclusão de notebook

--- modelo_logico_ddl.sql
+++ modelo_logico_ddl.sql
@@ -1,8 +1,10 @@
+/* logico_normalizado: */
+
 CREATE TABLE cliente (
     codigo_cliente INT PRIMARY KEY,
     nome_cliente VARCHAR(255),
     sobrenome_cliente VARCHAR(255),
-    telefone_cliente VARCHAR(15),
+    telefone_cliente VARCHAR(11),
     municipio_cliente VARCHAR(255),
     codigo_tipo_cliente INT
 );
@@ -11,14 +13,13 @@ CREATE TABLE produto (
     codigo_produto INT PRIMARY KEY,
     nome_produto VARCHAR(255),
     descricao_produto VARCHAR(255),
-    fk_diretor_responsavel_codigo_diretor_responsavel INT,
+    codigo_diretor_responsavel INT,
     codigo_tipo_produto INT
 );
 
 CREATE TABLE contrato (
-    codigo_contrato INT PRIMARY KEY,
-    fk_produto_codigo_produto INT,
-    fk_cliente_codigo_cliente INT
+    codigo_produto INT,
+    codigo_cliente INT
 );
 
 CREATE TABLE diretor_responsavel (
@@ -37,25 +38,24 @@ CREATE TABLE tipo_cliente (
     tipo_cliente VARCHAR(255)
 );
  
- 
 ALTER TABLE cliente ADD CONSTRAINT FK_cliente_2
     FOREIGN KEY (codigo_tipo_cliente)
     REFERENCES tipo_cliente (codigo_tipo_cliente);
  
 ALTER TABLE produto ADD CONSTRAINT FK_produto_2
-    FOREIGN KEY (fk_diretor_responsavel_codigo_diretor_responsavel)
+    FOREIGN KEY (codigo_diretor_responsavel)
     REFERENCES diretor_responsavel (codigo_diretor_responsavel);
  
 ALTER TABLE produto ADD CONSTRAINT FK_produto_3
     FOREIGN KEY (codigo_tipo_produto)
-    REFERENCES tipo_produto (tipo_produto);
+    REFERENCES tipo_produto (codigo_tipo_produto);
  
 ALTER TABLE contrato ADD CONSTRAINT FK_contrato_1
-    FOREIGN KEY (fk_produto_codigo_produto)
+    FOREIGN KEY (codigo_produto)
     REFERENCES produto (codigo_produto)
     ON DELETE RESTRICT;
  
 ALTER TABLE contrato ADD CONSTRAINT FK_contrato_2
-    FOREIGN KEY (fk_cliente_codigo_cliente)
+    FOREIGN KEY (codigo_cliente)
     REFERENCES cliente (codigo_cliente)
     ON DELETE RESTRICT;
